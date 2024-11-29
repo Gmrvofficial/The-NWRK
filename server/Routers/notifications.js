a9623a2cf1dc33ddf10351692e9f524ed5e126ce
Gmrvofficial <samuelbinvest@outlook.com>
2024-11-28 20:31:43 -0500

    create a delete and get route for notifications

--- server/Routers/notifications.js
+++ server/Routers/notifications.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const notification = require('../models/notifications')
 const AuthenticateTKN = require('../middleware/Auth')
+const message = require('../models/message')
 
 router.get('/', AuthenticateTKN, async (req,res)=>{
     
@@ -15,3 +16,15 @@ router.get('/', AuthenticateTKN, async (req,res)=>{
     }
 
 })
+ router.delete('/delete/:id',AuthenticateTKN,async (req,res)=>{
+    try{
+        const ID = req.params.id 
+        const deletedNotes = await notification.findByIdAndDelete(ID)
+        if(!deletedNotes){
+            res.status(404).json({message:'Notification not found'})
+        }
+
+    }catch{
+        res.status(500).json({message:'Unable to DELETE Notification'})
+    }
+ })
