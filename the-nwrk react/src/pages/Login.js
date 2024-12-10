6b5a644dc89fa824227f1faa9cbb20052395532b
Gmrvofficial <samuelbinvest@outlook.com>
2024-12-09 22:37:29 -0500

    correct error message for incorrect login creds:

--- the-nwrk react/src/pages/Login.js
+++ the-nwrk react/src/pages/Login.js
@@ -1,16 +1,18 @@
 import { useState } from "react"
 import nwrk from '../Assets/NWRK.png'
 import plug from '../Assets/plug.png'
-import { NavLink } from "react-router-dom"
+import { NavLink,useNavigate } from "react-router-dom"
 const LoginPage = ()=>{ 
+    
     const [Name,setUsername] = useState('')
     const [password,setPassword] = useState('')
     const [business, setBusiness] = useState("");
     const [niche, setNiche] = useState("");
     const [email,setEmail] =useState('');
     const[message,setMessage]=useState('')
+    const navigate =useNavigate()
     
-    
+
     const handleSubmit = async (e)=>{
         e.preventDefault()
         try{
@@ -25,18 +27,20 @@ const LoginPage = ()=>{
                     nicheType:niche,
                     email:email})
             })
-            const data = await response.json();
+            // let data = await response.json();
 
             if(!response.ok){
-                console.log('Unable to Login',data);
+            
+                console.log('Unable to Login');
                 throw new Error('Error Logging in')
                 
             }else{
-            setMessage({message:'Login Successfull',type:'success'})
+            setMessage({content:'Login Successfull',type:'success'})
+            navigate('/')
            }
         }catch(err){
             console.error(err);
-            setMessage({message:err.message,type:'error'})
+            setMessage({content:err.message,type:'error'}) 
         }
     }
 
@@ -44,15 +48,15 @@ const LoginPage = ()=>{
     return(
         <>
             <div className="border">
-                <div className="flex items-center justify-center min-h-screen">
+                <div className="flex items-center justify-center min-h-screen drop-shadow-xl">
                     <form className="" onSubmit={handleSubmit}>
                         <div className="bg-black p-20 rounded-md ">
                             <div className="flex items-center justify-center ">
-                                <div className="w-40"><img src={nwrk} alt="" /></div>
+                                <div className="w-40 brightness-200"><img src={nwrk} alt="" /></div>
 
                             </div>
                             <div className="flex items-center justify-center ">
-                                <div className="rotate-90 w-10 animate-pulse"><img src={plug} alt="" /></div>
+                                <div className="rotate-90 w-10 animate-pulse brightness-150"><img src={plug} alt="" /></div>
 
                             </div>
                             <div className="flex items-center justify-center p-6">
@@ -82,27 +86,25 @@ const LoginPage = ()=>{
 
                             <div className=" flex items-center justify-center p-3">
                                 <button 
-                                className="rounded-lg bg-sky-300 p-2 hover:text-sky-500 hover:bg-white hover:font-bold duration-150 ease-in-out"
+                                className="rounded-lg bg-sky-300 p-2 hover:text-sky-500 hover:bg-white hover:font-bold duration-150 ease-in-out hover:brightness-110"
                                 >
                                     Log In
                                 </button>
                             </div>
 
                             <div>
-                                {message.text && (
-                                    <div className={`${message.type === 'success' ? 'text-lime-400': 'text-rose-600' }`}> 
-                                    {message.text}
+                                {message.content && (
+                                    <div className={`text-center ${message.type === 'success' ? 'text-lime-400': 'text-rose-600' }`}> 
+                                    {message.content}
                                     </div>)}
-                                </div> 
+                            </div> 
                             
-
-                            </div>
-                            <div className="flex items-center justify-center p-2 ">
-                                <NavLink to='/register' className='hover:text-sky-300 duration-150 ease-in-out'> 
-                                Already have an account? Sign Up!
-                                </NavLink>
-                            </div>
-
+                        </div>
+                        <div className="flex items-center justify-center p-2 ">
+                            <NavLink to='/register' className='hover:text-sky-500 duration-150 ease-in-out'> 
+                                Don't have an account? Sign Up!
+                            </NavLink>
+                        </div>
                     </form>
 
                 </div>
--- the-nwrk react/src/pages/Register.js
+++ the-nwrk react/src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import plug from '../Assets/plug.png'
-import link from '../Assets/Link2.png'
-
+import plug from "../Assets/plug.png";
+import link from "../Assets/Link2.png";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -9,8 +9,8 @@ const RegisterPage = () => {
   const [business, setBusiness] = useState("");
   const [niche, setNiche] = useState("");
   const [username, setUsername] = useState("");
-
-
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate()
   const handleRegister = async (e) => {
     e.preventDefault();
 
@@ -23,85 +23,108 @@ const RegisterPage = () => {
           email: email,
           password: password,
           business: business,
-          niche: niche.split(','),
+          niche: niche.split(","),
           username: username,
         }),
+
       });
 
+      const data = await response.json();
       if (!response.ok) {
-        console.log("Unable to Register");
+        console.log("Unable to Register", data);
         throw new Error("Unable to Register");
       }
+
+      setMessage({ content: "Registration", type: "success" });
       console.log();
+      navigate('/')
     } catch (err) {
+      setMessage({ content: err.message, type: "error" });
       console.log(err);
     }
   };
 
-  return(
+  return (
     <>
-        <div className="border">
-            <div className="bg-zinc-100 flex items-center justify-center min-h-screen">
-                <form className="" onSubmit={handleRegister}>
-                    <div className="bg-black p-20 rounded-md ">
-                        <div className="flex items-center justify-center p-6">
-                            <div className="w-20"><img src={link} alt="" /></div>
-                        </div>
-                        <div className="flex items-center justify-center ">
-                                <div className="rotate-90 w-10 animate-pulse"><img src={plug} alt="" /></div>
+      <div className="border">
+        <div className=" flex items-center justify-center min-h-screen">
+          <form className="" onSubmit={handleRegister}>
+            <div className="bg-black p-20 rounded-md ">
+              <div className="flex items-center justify-center p-6">
+                <div className="w-20 brightness-150">
+                  <img src={link} alt="" />
+                </div>
+              </div>
+              <div className="flex items-center justify-center ">
+                <div className="rotate-90 w-10 animate-pulse brightness-150">
+                  <img src={plug} alt="" />
+                </div>
+              </div>
 
-                            </div>
+              <div className="flex items-center justify-center p-6">
+                <h1 className=" text-xl font-bold text-white">Sign Up</h1>
+              </div>
+              <div className="p-6 mx-6">
+                <input
+                  className="flex rounded-md p-1"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value.toLowerCase())
+                  }
+                />
+              </div>
+              <div className="p-6 mx-6">
+                <input
+                  className="flex rounded-md p-1"
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value.toLowerCase())}
+                />
+              </div>
+              <div className="p-6 mx-6">
+                <input
+                  className="rounded-md p-1 "
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
 
-                        <div className="flex items-center justify-center p-6">
-                            <h1 className=" text-xl font-bold text-white">Sign Up</h1>
-                        </div>
-                        <div className="p-6 mx-6">
-
-                            <input 
-                            className="flex rounded-md p-1"
-                            type="text" 
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e)=>setEmail(e.target.value)}/>
-
-                        </div>
-                        <div className="p-6 mx-6">
-
-                            <input 
-                            className="flex rounded-md p-1"
-                            type="text" 
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e)=>setUsername(e.target.value)}/>
-
-                        </div>
-                        <div className="p-6 mx-6">
-                            <input 
-                            className="rounded-md p-1 "
-                            type="password"
-                            placeholder="Password"
-                            value ={password}
-                            onChange={(e)=>setPassword(e.target.value)}
-                            />
-
-                        </div>
-
-                        <div className=" flex items-center justify-center p-3">
-                            <button 
-                            className="rounded-lg bg-sky-300 p-2 hover:animate-pulse"
-                            >
-                                Sign Up
-                            </button>
-                        </div>
-
-                    </div>
-
-                </form>
-
+              <div className=" flex items-center justify-center p-3">
+                <button className="rounded-lg bg-sky-300 p-2 hover:text-sky-500 hover:bg-white hover:font-bold duration-150 ease-in-out hover:brightness-110">
+                  Sign Up
+                </button>
+              </div>
+              <div>
+                {message.content && (
+                  <div
+                    className={`${
+                      message.content === "success"
+                        ? "text-lime-400"
+                        : "text-rose-600"
+                    }`}
+                  >
+                    {message.content}
+                  </div>
+                )}
+              </div>
             </div>
+            <div className="flex items-center justify-center p-2 ">
+              <NavLink
+                to="/login"
+                className="hover:text-sky-500 duration-150 ease-in-out"
+              >
+                Already have an account? Log In!
+              </NavLink>
+            </div>
+          </form>
         </div>
+      </div>
     </>
-)
+  );
 };
 
 export default RegisterPage;
